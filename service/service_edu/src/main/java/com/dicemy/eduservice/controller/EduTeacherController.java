5557ed4af593bbdf9ac12f93562d65b505e221f9
Dicemy <1583925267@qq.com>
2022-12-08 20:34:13 +0800

    使用代码生成器生成service-teacher相关代码

---
+++ service/service_edu/src/main/java/com/dicemy/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.dicemy.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author dicemy
+ * @since 2022-12-08
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+}
+
---
+++ service/service_edu/src/main/java/com/dicemy/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.dicemy.eduservice.mapper;
+
+import com.dicemy.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author dicemy
+ * @since 2022-12-08
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
